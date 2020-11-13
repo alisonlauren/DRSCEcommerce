71b7271d809b61f69834eceae78d5dd99b3f3cd5
Alison Manning <alisonlaurenmanning@gmail.com>
2020-11-13 15:24:37 -0600

    add

---
+++ drsc_backend/data.js
@@ -0,0 +1,74 @@
+
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'berry soap',
+            category: 'handmade soap',
+            image: './images/soap1.jpg',
+            images: '../images/soap1.jpg',
+            price: 120,
+            rating: 3,
+            numReviews: 1,
+            description: 'made with x, y, z'
+        },
+        {
+            _id: '2',
+            name: 'berry soap',
+            category: 'handmade soap',
+            image: './images/soap1.jpg',
+            images: '../images/soap1.jpg',
+            price: 120,
+            rating: 4.5,
+            numReviews: 1,
+            description: 'made with x, y, z'
+        },
+        {
+            _id: '3',
+            name: 'berry soap',
+            category: 'handmade soap',
+            image: './images/soap1.jpg',
+            images: '../images/soap1.jpg',
+            price: 120,
+            rating: 4.2,
+            numReviews: 44,
+            description: 'made with x, y, z'
+        },
+        {
+            _id: '4',
+            name: 'berry soap',
+            category: 'handmade soap',
+            image: './images/soap1.jpg',
+            images: '../images/soap1.jpg',
+            images: '../images/soap1.jpg',
+            price: 120,
+            rating: 2.5,
+            numReviews: 32,
+            description: 'made with x, y, z'
+        },
+        {
+            _id: '5',
+            name: 'berry soap',
+            category: 'handmade soap',
+            image: './images/soap1.jpg',
+            images: '../images/soap1.jpg',
+            price: 120,
+            rating: 4.2,
+            numReviews: 55,
+            description: 'made with x, y, z'
+        },
+        {
+            _id: '6',
+            name: 'berry soap',
+            category: 'handmade soap',
+            image: './images/soap1.jpg',
+            images: '../images/soap1.jpg',
+            price: 120,
+            rating: 5,
+            numReviews: 2,
+            description: 'made with x, y, z'
+        },
+    ],
+};
+
+export default data;
---
+++ drsc_frontend/src/components/Product.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import Rating from './Rating';
+import { Link } from 'react-router-dom';
+
+export default function Product(props) {
+    const { product } = props;
+    return (
+            <div key={product._id} className="card">
+                
+                <img className="medium" src={product.image} alt={product.name} />
+                <div className="card-body">
+                    <a href={`/product/${product.id}`}>
+                    <Link to= {`/product/${product._id}`}>{product.name}</Link>
+                        
+                    </a>
+                    <Rating rating={product.rating} numReviews={product.numReviews} />
+            <div className="price">${product.price}</div>
+        </div>
+        </div>
+    )
+}
+
+
+ 
+
+
+
+
+
+
+
+
+
