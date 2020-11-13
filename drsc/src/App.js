971a9266d7e404fea15d011cf2f32cb5933fa776
Alison Manning <alisonlaurenmanning@gmail.com>
2020-11-13 12:00:44 -0600

    add

---
+++ drsc/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
+function App() {
+  return (
+    <BrowserRouter>
+    <div className="grid-container">
+      <header className="row">
+        <div>
+          <a className="brand" href="/">
+            Trish Site
+        </a>
+        </div>
+        <div>
+          <a href="/cart">Cart</a>
+          <a href="/signin">Sign In</a>
+        </div>
+      </header>
+      <main>
+        <Route path="/product/:id" component={ProductScreen} />
+        <Route exact path="/" component={HomeScreen}  />
+        
+      </main>
+      <footer className="row center">All rights reserved</footer>
+      </div>
+      </BrowserRouter>
+  );
+}
+
+export default App;
+
+
