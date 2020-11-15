06b77ae529ca77ce05e61075671ed66e39f99fca
Alison Manning <alisonlaurenmanning@gmail.com>
2020-11-15 14:57:46 -0600

    add

--- drsc_frontend/src/index.js
+++ drsc_frontend/src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import store from './store';
 
 
 ReactDOM.render(
+  <Provider store={store}>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
