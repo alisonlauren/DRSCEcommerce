71c6d5ce5272cd0c6e367a3b873f141831643f14
Alison Manning <alisonlaurenmanning@gmail.com>
2020-11-19 15:28:03 -0600

    add

--- drsc_backend/server.js
+++ drsc_backend/server.js
@@ -43,6 +43,10 @@ app.use((err, req, res, next) => {
 
 })
 
+if (process.env.NODE_ENV === 'production') {
+    app.user(express.static('client/build'));
+}
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Serve at http://localhost:${port}`)
