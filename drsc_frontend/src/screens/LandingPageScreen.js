633e68279d3fc25560288281ec4a3a4eb25c58e6
Alison Manning <alisonlaurenmanning@gmail.com>
2020-11-28 15:42:31 -0600

    add

--- drsc_frontend/src/screens/LandingPageScreen.js
+++ drsc_frontend/src/screens/LandingPageScreen.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
 
+
 export default function LandingPageScreen() {
     return (
-        <div>
-            <img className="backwood" class="ui fluid image" 
-            src="https://trickvilla.com/wp-content/uploads/Wood-Background-Images.jpg"></img>
+        <div className="container1">
+            
+            <img className="logo" 
+            src="images/woodlogo.gif"></img>
         </div>
     )
 }
